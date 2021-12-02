f941718112270854c5960237d3951500a8a8085b
gunasekhar9 <gunasekhar1539@gmail.com>
2021-12-02 11:51:00 +0530

    [gunasekhar9] add : Reflection program is done

---
+++ Reflection/Reflection/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReflectionTest reflect = new ReflectionTest();
+            reflect.Test();
+        }
+    }
+}
